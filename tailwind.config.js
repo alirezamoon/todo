11f7bb46ab97ce1e0f1d510ab894c99aafaf71f3
alireza khanamani <khanamani1998@gmail.com>
2022-12-04 23:49:18 +0330

    config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        olive: {
+          main: "#907c74",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
